cc07c011df3c4b61a5131f77e735f172dc4dcb28
mikibakaiki <joaomiguelcampos6@gmail.com>
2024-05-21 14:08:50 +0100

    seems to be functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import KimonoCard from "./components/KimonoCard"; // Create a KimonoCard component for displaying kimono details
 import KimonoGraph from "./components/KimonoGraph";
 import SearchBar from "./components/SearchBar"; // Create a KimonoGraph component for displaying price history
@@ -14,7 +14,8 @@ import EmptyCard from "./components/EmptyCard";
 function App() {
   const [kimonos, setKimonos] = useState<Kimono[]>([]); // State to store kimono data
   const [selectedKimono, setSelectedKimono] = useState<Kimono | null>(null); // State for selected kimono
-  const [searchQuery, setSearchQuery] = useState<string>(""); // State for search query
+  const [inputValue, setInputValue] = useState<string>(""); // New state for immediate input update
+  const [searchQuery, setSearchQuery] = useState<string>("");
   const [isLoading, setIsLoading] = useState(false); // State for loading status
   const [showLoader, setShowLoader] = useState(true); // Flag to control when the loader should be displayed
   const [page, setPage] = useState(1); // State for pagination
@@ -39,69 +40,75 @@ function App() {
       setEmptyCardsCount(20); // Display 20 empty cards while fetching more data
     }
     setIsLoading(true);
-    try {
-      const data = await getKimonos<Kimono[]>("kimonos", page, 20, searchQuery);
-      const processedData = preprocessKimonosDates(data);
-      if (page === 1) {
-        setKimonos(processedData);
-      } else {
-        setKimonos((prevKimonos) => [...prevKimonos, ...processedData]);
+    setTimeout(async () => {
+      try {
+        const data = await getKimonos<Kimono[]>(
+          "kimonos",
+          page,
+          20,
+          searchQuery
+        );
+        const processedData = preprocessKimonosDates(data);
+        if (page === 1) {
+          setKimonos(processedData);
+        } else {
+          setKimonos((prevKimonos) => [...prevKimonos, ...processedData]);
+        }
+        setHasMoreKimonos(data.length === 20);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      } finally {
+        if (page === 1 || searchQuery) {
+          setShowLoader(false);
+        } else {
+          setIsInfiniteScrollLoading(false);
+          setEmptyCardsCount(0); // Hide empty cards after data is fetched
+        }
       }
-      setHasMoreKimonos(data.length === 20);
-    } catch (error) {
-      console.error("Failed to fetch data:", error);
-    } finally {
-      if (page === 1 || searchQuery) {
-        setShowLoader(false);
-      } else {
-        setIsInfiniteScrollLoading(false);
-        setEmptyCardsCount(0); // Hide empty cards after data is fetched
-      }
-    }
+    }, 1000);
   };
 
-  // Debounce Kimonos search query to avoid making too many requests in a short period of time
-  const debouncedFetch = debounce(fetchKimonos, 1000);
-
-  const loadMoreItems = () => {
-    if (!isLoading && hasMoreKimonos) {
-      setPage((prevPage) => prevPage + 1);
-    }
-  };
-
-  // Debounce the loadMoreItems function so it only triggers once after a specified delay
-  const debouncedLoadMoreItems = debounce(loadMoreItems, 100);
-
-  const handleKimonoCardClick = (kimono: Kimono) => {
-    setSelectedKimono(kimono);
-  };
-
-  const handleSearchChange = (value: string) => {
-    setSearchQuery(value);
-    setPage(1);
-  };
-
-  // scroll event listener to fetch more kimonos when the user reaches the bottom of the page
+  // scroll event listener to fetch more kimonos when the user 
+  // reaches the bottom of the page
   useEffect(() => {
     const handleScroll = () => {
-      const bottom = window.scrollY + window.innerHeight >= document.documentElement.scrollHeight - 200;
+      const bottom =
+        window.scrollY + window.innerHeight >=
+        document.documentElement.scrollHeight - 200;
       if (bottom && !isInfiniteScrollLoading && hasMoreKimonos) {
-        console.log('Reached bottom, loading more items');
+        console.log("Reached bottom, loading more items");
         setPage((prevPage) => prevPage + 1);
       }
     };
-  
+
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, [isInfiniteScrollLoading, hasMoreKimonos]);
-  
-  
+
   useEffect(() => {
     fetchKimonos();
   }, [searchQuery, page]);
-  
+
+  // Debounced function to update searchQuery
+  const debouncedSearch = useCallback(
+    debounce((value: string) => {
+      setSearchQuery(value);
+      setPage(1);
+    }, 300),
+    []
+  );
+
+  const handleKimonoCardClick = (kimono: Kimono) => {
+    setSelectedKimono(kimono);
+  };
+
+  // Immediate update for input value
+  const handleInputChange = (value: string) => {
+    setInputValue(value);
+    debouncedSearch(value);
+  };
 
   if (showLoader) {
     return (
@@ -118,7 +125,7 @@ function App() {
         <div className="header-title">Brazilian Jiu-Jitsu Kimonos</div>
         {/* Search bar and filter options */}
         <div className="search-bar-container">
-          <SearchBar value={searchQuery} onChange={handleSearchChange} />
+          <SearchBar value={inputValue} onChange={handleInputChange} />
         </div>
       </header>
       {/* Kimono Cards */}
