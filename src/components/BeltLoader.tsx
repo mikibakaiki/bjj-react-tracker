38c49d444f5f7ee94c343a212421b8100bc8c047
mikibakaiki <joaomiguelcampos6@gmail.com>
2024-05-21 10:45:02 +0100

    belt loader component

---
+++ src/components/BeltLoader.tsx
@@ -0,0 +1,11 @@
+import "./BeltLoader.css";
+
+const BeltLoader = () => {
+  return (
+    <div className="belt-slider">
+      <div className="belt-slide"></div>
+    </div>
+  );
+};
+
+export default BeltLoader;
